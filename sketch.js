add8443c0aa787569205d9f777acd0de9ee8dc96
3013484 <70283829+3013484@users.noreply.github.com>
2020-11-01 12:32:23 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,10 +3,16 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+const Render = Matter.Render;
+var tree, boy, mango, stone;
+var launcher;
+var mango1, mango2, mango3, mango4, mango5;
+var world;
+var launchingForce = 100;
 
-function preload()
-{
-	
+function preload() {
+	boy = loadImage("sprites/boy.png");	
 }
 
 function setup() {
@@ -17,7 +23,7 @@ function setup() {
 	world = engine.world;
 
 	//Create the Bodies Here.
-
+	
 
 	Engine.run(engine);
   
